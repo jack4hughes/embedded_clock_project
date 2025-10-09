e181b7f4ca19213336824e667cd73668ab59c416
Jack Hughes <jack4.hughes@live.uwe.ac.uk>
2025-10-09 18:42:23 +0100

    Implemented add_page to add new pages to the PageStateMachine

--- include/page.h
+++ include/page.h
@@ -1,12 +1,15 @@
+#ifndef PAGE_H
+#define PAGE_H
 #include "stdint.h"
 #include "stdio.h"
+
 #define MAX_NUMBER_OF_PAGES 16
 
 typedef struct Page {
-  void (*timer_update_fn)(void* context);
-  void (*input_update_fn)(void* context, char input);
-  void (*display_fn)(void* context);
-  void (*init_function);
+  void (*timer_update_fn)(char *time_string); // Time string created by OS, so we need to pass this as a function. 
+  void (*input_update_fn)(char input);
+  void (*draw_fn)(); //Doesnt need bitmap location as this is a signleton.
+  void (*init_fn)();
   void* page_data;
 } Page;
 
@@ -25,3 +28,5 @@ Page *get_next_page();
 
 int add_page(Page *page_loc);
 //Allows a new page to be added to the PageStateMachine, and returns the index.
+#endif
+
--- src/page.c
+++ src/page.c
@@ -7,7 +7,9 @@ Page *get_active_page_loc() {
 }
 
 Page *get_next_page() {
+    //TODO: Check This for rewrites.
   if (active_page_selector.number_of_pages == 0) {
+    printf("No pages have been created yet! \n");
     return NULL; //error when no pages are set.
   }
   
@@ -17,14 +19,22 @@ Page *get_next_page() {
   if (
     active_page_selector.active_index < active_page_selector.number_of_pages
   ) {
-    Page *page_loc = get_active_page_loc();
+    active_page_selector.active_index++;
+    int index = active_page_selector.active_index; //just for readability
+    active_page_selector.active_page = active_page_selector.pages[index];
+    
+    Page *page_loc = active_page_selector.pages[index];
+    active_page_selector.active_page = page_loc;
+    printf("returning next page!, page loc: %x", (int) page_loc);
     return page_loc;
   }
   
   // Case where we are at the last active page. 
   else {
     active_page_selector.active_index = 0;
+    active_page_selector.active_page = active_page_selector.pages[0]; 
     Page *page_loc = get_active_page_loc();
+    printf("returning first page!, page loc: %x\n", (int) page_loc);
     return page_loc;
   }
 }
@@ -34,7 +44,12 @@ int init_page_state_machine() {
 }
 
 int add_page(Page *page_loc) {
+  printf("current active pages: %x\n, page to add: %x\n", (int) active_page_selector.number_of_pages, page_loc);
+  Page *page_array_loc = &active_page_selector.pages;
+  Page *new_page_loc = page_array_loc + active_page_selector.number_of_pages; //gets the postiion of the last page.
+  printf("page array start: %x, writing to: %x\n", (int) page_array_loc, new_page_loc);
+  active_page_selector.pages[active_page_selector.number_of_pages] = page_loc; 
+
   active_page_selector.number_of_pages++;
-  active_page_selector.pages[active_page_selector.number_of_pages - 1] = page_loc; //this horrible line of code writes the page loc to our page list.
   return 0;
 }
